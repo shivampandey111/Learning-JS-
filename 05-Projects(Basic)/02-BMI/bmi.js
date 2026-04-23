4042d2dfee76a8fed125d786df5727e0113ac6cc
shivampandey111 <shivampandey33033@gmail.com>
2026-04-23 13:41:14 +0000

    Project 02 (BMI)

---
+++ 05-Projects(Basic)/02-BMI/bmi.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if(height === '' || height<0 || isNaN(height)){
+        results.innerHTML = 'Please enter a valid height.'
+    }
+    else if(weight === '' || weight<0 || isNaN(weight)){
+        results.innerHTML = 'Please enter a valid weight.'
+    }
+    else{
+        const bmi = (weight/((height*height)/10000)).toFixed(2);
+        if(bmi<18.6){
+            results.innerHTML = `<span>${bmi} <br>
+            Under Weight</span>`;
+        }
+        else if(bmi>18.6 && bmi<24.9){
+            results.innerHTML = `<span>${bmi} <br>
+            Normal Range</span>`;
+        }
+        else{
+            results.innerHTML = `<span>${bmi} <br>
+            Over Weight</span>`;
+        }
+    }
+
+
+
+
+})
